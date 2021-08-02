8612239ec1ea823ef8170dfbb6715ed8b7369fad
Frode Carlsen <frode.odde.carlsen@nav.no>
2021-08-02 08:24:22 +0200

    Initial version: includes transformations for package names, xml namespaces, and selected pom artifactIds  along with suggestions for updating pom versions by script, and initial documentation

---
+++ src/main/java/no/nav/k9/dev/jakarta/App.java
@@ -0,0 +1,42 @@
+package no.nav.k9.dev.jakarta;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.transformer.Transformer;
+import org.eclipse.transformer.Transformer.AppOption;
+
+/**
+ * Transform a given code base to jakarta with provided default transformation rules
+ * 
+ * (adapted from JakartaTransformer in eclipse-transformer.cli project)
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        var transformer = new Transformer(System.out, System.err);
+        transformer.setOptionDefaults(App.class, getOptionDefaults());
+        transformer.setArgs(args);
+        int rc = transformer.run();
+        System.exit(rc);
+    }
+
+    public static final String DEFAULT_RENAMES_REFERENCE = "jakarta-renames.properties";
+    public static final String DEFAULT_VERSIONS_REFERENCE = "jakarta-versions.properties";
+    public static final String DEFAULT_BUNDLES_REFERENCE = "jakarta-bundles.properties";
+    public static final String DEFAULT_DIRECT_REFERENCE = "jakarta-direct.properties";
+    public static final String DEFAULT_MASTER_TEXT_REFERENCE = "jakarta-text-master.properties";
+    public static final String DEFAULT_PER_CLASS_CONSTANT_MASTER_REFERENCE = "jakarta-per-class-constant-master.properties";
+
+    public static Map<Transformer.AppOption, String> getOptionDefaults() {
+        Map<Transformer.AppOption, String> optionDefaults = new HashMap<>();
+
+        optionDefaults.put(AppOption.RULES_RENAMES, DEFAULT_RENAMES_REFERENCE);
+        optionDefaults.put(AppOption.RULES_VERSIONS, DEFAULT_VERSIONS_REFERENCE);
+        optionDefaults.put(AppOption.RULES_BUNDLES, DEFAULT_BUNDLES_REFERENCE);
+        optionDefaults.put(AppOption.RULES_DIRECT, DEFAULT_DIRECT_REFERENCE);
+        optionDefaults.put(AppOption.RULES_MASTER_TEXT, DEFAULT_MASTER_TEXT_REFERENCE);
+        optionDefaults.put(AppOption.RULES_PER_CLASS_CONSTANT, DEFAULT_PER_CLASS_CONSTANT_MASTER_REFERENCE);
+
+        return optionDefaults;
+    }
+}
