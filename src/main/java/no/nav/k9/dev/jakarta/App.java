5f13f0e5fe68096659950623ec1e04282e99f265
Frode Carlsen <frode.odde.carlsen@nav.no>
2022-01-07 07:49:12 +0000

    Oppgradert siste eclipse-transformer. Ryddet overflødige properties filer

--- src/main/java/no/nav/k9/dev/jakarta/App.java
+++ src/main/java/no/nav/k9/dev/jakarta/App.java
@@ -1,10 +1,17 @@
 package no.nav.k9.dev.jakarta;
 
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
 
 import org.eclipse.transformer.Transformer;
 import org.eclipse.transformer.AppOption;
+import org.eclipse.transformer.cli.JakartaTransformerCLI;
+import org.eclipse.transformer.Transformer.ResultCode;
+import org.eclipse.transformer.jakarta.JakartaTransform;
+import org.slf4j.LoggerFactory;
 
 /**
  * Transform a given code base to jakarta with provided default transformation rules
@@ -14,30 +21,22 @@ import org.eclipse.transformer.AppOption;
 public class App {
     
     public static void main(String[] args) throws Exception {
-        var transformer = new Transformer(System.out, System.err);
-        transformer.setOptionDefaults(App.class, getOptionDefaults());
-        transformer.setArgs(args);
-        int rc = transformer.run();
-        System.exit(rc);
+        var cli = new JakartaTransformerCLI(System.out, System.err, args);
+        cli.setOptionDefaults(getLoader(), JakartaTransform.getOptionDefaults());
+        
+        var rc = JakartaTransformerCLI.runWith(cli);
+        System.exit(rc.equals(Transformer.ResultCode.SUCCESS_RC) ? 0 : 1);
     }
 
-    public static final String DEFAULT_RENAMES_REFERENCE = "jakarta-renames.properties";
-    public static final String DEFAULT_VERSIONS_REFERENCE = "jakarta-versions.properties";
-    public static final String DEFAULT_BUNDLES_REFERENCE = "jakarta-bundles.properties";
-    public static final String DEFAULT_DIRECT_REFERENCE = "jakarta-direct.properties";
-    public static final String DEFAULT_MASTER_TEXT_REFERENCE = "jakarta-text-master.properties";
-    public static final String DEFAULT_PER_CLASS_CONSTANT_MASTER_REFERENCE = "jakarta-per-class-constant-master.properties";
+    public static Set<String> LOCALS = Set.of("jakarta-renames.properties", "jakarta-direct.properties", "jakarta-text-master.properties");
 
-    public static Map<AppOption, String> getOptionDefaults() {
-        Map<AppOption, String> optionDefaults = new HashMap<>();
-
-        optionDefaults.put(AppOption.RULES_RENAMES, DEFAULT_RENAMES_REFERENCE);
-        optionDefaults.put(AppOption.RULES_VERSIONS, DEFAULT_VERSIONS_REFERENCE);
-        optionDefaults.put(AppOption.RULES_BUNDLES, DEFAULT_BUNDLES_REFERENCE);
-        optionDefaults.put(AppOption.RULES_DIRECT, DEFAULT_DIRECT_REFERENCE);
-        optionDefaults.put(AppOption.RULES_MASTER_TEXT, DEFAULT_MASTER_TEXT_REFERENCE);
-        optionDefaults.put(AppOption.RULES_PER_CLASS_CONSTANT, DEFAULT_PER_CLASS_CONSTANT_MASTER_REFERENCE);
-
-        return optionDefaults;
+    public static Function<String, URL> getLoader(){
+        return (key) -> {
+             if(LOCALS.contains(key)){
+                 return App.class.getResource(key);
+             } else {
+                 return JakartaTransform.getRuleLoader().apply(key);
+             }
+        };
     }
 }
